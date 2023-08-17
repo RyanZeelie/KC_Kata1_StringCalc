ee2229e8f953cdeded77ed13fbc084a0d33346af
RyanZeelie <ryan.zeelie@kaleidocode.co.za>
2023-08-17 19:02:27 +0800

    Removed unnecessary tests

--- StringCalculatorTests/AdditionTests.cs
+++ StringCalculatorTests/AdditionTests.cs
@@ -33,45 +33,10 @@ namespace StringCalculatorTests
         }
 
         [Test]
-        public void GIVEN_TwoNumbersAsString_WHEN_AddMethodCalled_SHOULD_AddNumbersTogether()
+        public void GIVEN_AlistOfNumbers_WHEN_AddMethodCalled_SHOULD_AddNumbersTogether()
         {
             // Arrange
             var testInput = "1,2";
-            var expectedResult = 3;
-
-            _numberService.ParseNumbers(testInput).Returns(new List<int> { 1, 2 });
-
-            //Act
-            var result = _stringCalculator.Add(testInput);
-
-            //Assert
-            Assert.That(result, Is.EqualTo(expectedResult));
-        }
-
-        [Test]
-        public void GIVEN_AnUnknownAmountOfNumbers_WHEN_AddMethodCalled_SHOULD_AddNumbersTogether()
-        {
-            // Arrange
-            var rndm = new Random();
-            var randomAmountOfNumbersBetween1And20 = Enumerable.Range(1, rndm.Next(20)).ToList();
-
-            var testInput = string.Join(",", randomAmountOfNumbersBetween1And20); 
-            var expectedResult = randomAmountOfNumbersBetween1And20.Sum();;
-
-            _numberService.ParseNumbers(testInput).Returns(randomAmountOfNumbersBetween1And20);
-
-            //Act
-            var result = _stringCalculator.Add(testInput);
-
-            //Assert
-            Assert.That(result, Is.EqualTo(expectedResult));
-        }
-        
-        [Test]
-        public void GIVEN_NumbersSeparatedByNewLines_WHEN_AddMethodCalled_SHOULD_AddNumbersTogether()
-        {
-            // Arrange
-            var testInput = "1\n2";
             var expectedResult = 3;
 
             _numberService.ParseNumbers(testInput).Returns(new List<int> { 1, 2 });
