f87954b5c6933d7350c5c46c01f5083c5a68cf34
RyanZeelie <ryan.zeelie@kaleidocode.co.za>
2023-08-21 10:15:15 +0800

    Add method handles new lines between numbers

--- StringCalculatorTests/NumberServiceTests.cs
+++ StringCalculatorTests/NumberServiceTests.cs
@@ -25,5 +25,19 @@ namespace StringCalculatorTests
             //Assert
             Assert.That(result, Is.EqualTo(expectedResult));
         }
+
+        [Test]
+        public void GIVEN_StringOfTwoNumbersSeparatedByANewLine_WHEN_ParsingNumbers_RETURNS_NumbersAsListInt()
+        {
+            // Arrange
+            var testInput = "1\n2";
+            var expectedResult = new List<int>() { 1, 2 };
+
+            //Act
+            var result = _numberService.ParseNumbers(testInput);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
     }
 }
